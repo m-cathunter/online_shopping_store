07cda9c7da20a3092d221ddbcdb8a2eea53e3061
Magdalena Kotołowska <m.kotolowska@gmail.com>
2019-04-16 08:43:17 +0200

    Clean up create produscts migration

--- db/migrate/20190414154943_create_products.rb
+++ db/migrate/20190414154943_create_products.rb
@@ -1,9 +1,9 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-        t.string :product_code
-        t.string :name
-        t.decimal :price, precision: 15, scale: 2
+      t.string :product_code
+      t.string :name
+      t.decimal :price, precision: 15, scale: 2
 
       t.timestamps
     end
