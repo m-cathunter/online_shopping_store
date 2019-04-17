0ea672bbd57722a223dd0c727cc3227b74288422
Magdalena Kotołowska <m.kotolowska@gmail.com>
2019-04-17 18:24:48 +0200

    Define price for product method on order item model

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,2 +1,29 @@
 class OrderItem < ApplicationRecord
+  belongs_to :order
+  belongs_to :product
+
+  def price_for_product
+    case self.product.discount
+    when 'no-discount'
+      default_price_counting
+    when 'buy-one-get-one-free'
+      if self.quantity.even?
+        default_price_counting / 2
+      else
+        ((self.quantity+1) * self.price) /2
+      end
+    when '3 or more'
+      if self.quantity >= 3
+        self.quantity * (self.price = 4.50)
+      else
+        default_price_counting
+      end
+    end
+  end
+
+  private
+
+  def default_price_counting
+    self.quantity * self.price
+  end
 end
