fcd0e42f06a36304fd3291f235fe86a77e386df8
Magdalena Kotołowska <m.kotolowska@gmail.com>
2019-04-17 16:06:11 +0200

    Configure routes by adding new paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#show'
-  resources :products, only: [:index]
+  resources :products
+
+  get '/cart', to: 'order_items#index'
+  resources :order_items, path: '/cart/order_items'
 end
