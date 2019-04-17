2254af33a940ea7f4c7b4bca475538f93dbacd65
Magdalena Kotołowska <m.kotolowska@gmail.com>
2019-04-17 18:21:48 +0200

    Add product controller and new product page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,24 @@ class ProductsController < ApplicationController
   def index
     @products = Product.order(:name)
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to products_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:product_code, :name, :price, :discount)
+  end
 end
