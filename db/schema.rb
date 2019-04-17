6904c4ae95d2ee294aa6408b7103a88a5d3d5401
Magdalena Kotołowska <m.kotolowska@gmail.com>
2019-04-17 18:43:24 +0200

    Update db schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_185814) do
+ActiveRecord::Schema.define(version: 2019_04_16_211907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,10 @@ ActiveRecord::Schema.define(version: 2019_04_15_185814) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "order_id", null: false
+    t.bigint "product_id"
+    t.integer "price_for_product"
     t.index ["order_id"], name: "index_order_items_on_order_id"
+    t.index ["product_id"], name: "index_order_items_on_product_id"
   end
 
   create_table "orders", force: :cascade do |t|
@@ -30,6 +33,9 @@ ActiveRecord::Schema.define(version: 2019_04_15_185814) do
     t.string "address"
     t.string "email"
     t.string "phone"
+    t.string "token"
+    t.decimal "total_price", precision: 15, scale: 2
+    t.string "status", default: "cart"
   end
 
   create_table "products", force: :cascade do |t|
@@ -38,7 +44,9 @@ ActiveRecord::Schema.define(version: 2019_04_15_185814) do
     t.decimal "price", precision: 15, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "discount"
   end
 
   add_foreign_key "order_items", "orders"
+  add_foreign_key "order_items", "products"
 end
