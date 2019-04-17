0387747e5b4a9e9dc7836497b0b9d7064f30ccf4
Magdalena Kotołowska <m.kotolowska@gmail.com>
2019-04-17 16:07:44 +0200

    Create order items controller and define methods

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,10 +1,20 @@
 class OrderItemsController < ApplicationController
+
+  def index
+    @order_items = current_cart.order.order_items
+  end
+
   def create
-    current_cart.add_item(
+    current_cart.add_order_item(
       product_id: params[:product_id],
       quantity: params[:quantity]
     )
 
+    redirect_to cart_path
+  end
+
+  def destroy
+    current_cart.remove_order_item(id: params[:id])
     redirect_to cart_path
   end
 end
