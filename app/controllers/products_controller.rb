a2ab30e0950b8e3604d9641e51b35e362d8418d6
Magdalena Kotołowska <m.kotolowska@gmail.com>
2019-04-18 08:25:00 +0200

    Create edit update and destroy method in products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,6 +17,30 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+
+    if @product.destroy
+      redirect_to products_path
+    else
+      redirect_to products_path
+    end
+  end
+
   private
 
   def product_params
