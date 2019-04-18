e88601e845e777f1d7dc05952e23fa047ef24f55
Magdalena Kotołowska <m.kotolowska@gmail.com>
2019-04-18 21:47:13 +0200

    Create admin pages for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,49 +1,6 @@
 class ProductsController < ApplicationController
+
   def index
     @products = Product.order(:name)
-  end
-
-  def new
-    @product = Product.new
-  end
-
-  def create
-    @product = Product.new(product_params)
-
-    if @product.save
-      redirect_to products_path
-    else
-      render 'new'
-    end
-  end
-
-  def edit
-    @product = Product.find(params[:id])
-  end
-
-  def update
-    @product = Product.find(params[:id])
-
-    if @product.update(product_params)
-      redirect_to products_path
-    else
-      render 'edit'
-    end
-  end
-
-  def destroy
-    @product = Product.find(params[:id])
-
-    if @product.destroy
-      redirect_to products_path
-    else
-      redirect_to products_path
-    end
-  end
-
-  private
-
-  def product_params
-    params.require(:product).permit(:product_code, :name, :price, :discount)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'welcome#show'
-  resources :products
+  namespace :admin do
+    resources :products, except: [:show]
+  end
+
+  resources :products, only: [:index]
 
   get '/cart', to: 'order_items#index'
   resources :order_items, path: '/cart/order_items'
